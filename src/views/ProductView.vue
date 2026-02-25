bf9103b764765353b75b78df4a3cc2f644379927
Paula Gajardo <paulagajardosch@gmail.com>
2026-02-25 18:53:04 -0300

    ajustando rutas

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-<p>Esta es la vista de producto</p>
+        <h1>{{ product.name }}</h1>
+ 
+ 
+        <router-link to="'/productos'">Volver</router-link>
     </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-const route = useRouter
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
 
+const products = [
+    { id: 1, name: "Espumante" },
+    { id: 2, name: "Vino" },
+    { id: 3, name: "Pisco" }
+
+]
+const product = computed(() => products.find((item) => item.id == route.params.id))
 
 </script>
 
-<style scoped>
 
-</style>
+<style scoped></style>
